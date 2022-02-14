4977a4a8599071ccc66a9432e760a55e6b94a078
Abdel Blkr <abdelouhab.belkhiri@etu.unice.fr>
2022-02-14 19:48:03 +0100

    Fix some bugs

--- scenarios/generated/test.js
+++ scenarios/generated/test.js
@@ -1,70 +1,70 @@
 
-//groomet app generated : dsl
-import { Grommet, Box, Heading, Tabs, Tab, Image, Text } from 'grommet';
+            //groomet app generated : dsl
+            import { Grommet, Box, Heading, Tabs, Tab, Image, Text } from 'grommet'; 
 
-import { statscovid, statlicenciement } from './data/data'
+                import { statscovid, statlicenciement } from './data/data' 
 
-export const WidgetClassic = ({ data }) => (
-    <Box round pad="medium" direction="column" background="#EEEEEE">
-        <Box height="xsmall" width="xsmall">
-            <Image fit="cover" src={data.icon_url} /> </Box>
-        <Heading alignSelf="center" level="2" margin="none" size="small"> {data.title} </Heading>
-        <Text alignSelf="center" size="90px" weight="bold"> {data.value} </Text></Box>
+            export const WidgetClassic= ({ data }) => (
+<Box round pad="medium" direction="column" background="#EEEEEE"> 
+<Box height="xsmall" width="xsmall">
+<Image fit="cover" src={data.icon_url}/> 
+ </Box>
+<Heading alignSelf="center" level="2" margin="none" size="small"> {data.title} </Heading>
+<Text alignSelf="center" size="90px" weight="bold"> {data.value} </Text> 
+ </Box>
 );
-const AppBar = (props) => (
-    <Box
-        tag='header'
-        direction='row'
-        align='center'
-        justify='between'
-        background='brand'
-        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-        elevation='medium'
-        style={{ zIndex: '1' }}
-        {...props}
-    />
-);
-const MyMenu = (props) => (
-    <Tabs>
-        <Tab title="pageOne">
-            <Box name="widgetWrapperOne"
-                width="100">
-                <WidgetClassic data={statscovid} />
-            </Box>
-        </Tab>
-        <Tab title="pageTwo">
-            <Box name="widgetWrapperOne"
-                width="100">
-                <WidgetClassic data={statlicenciement} />
-            </Box>
-        </Tab>
-    </Tabs>
+             const AppBar = (props) => (
+            <Box
+                tag='header'
+                direction='row'
+                align='center'
+                justify='between'
+                background='brand'
+                pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+                elevation='medium'
+                style={{ zIndex: '1' }}
+                {...props}
+            />
+        ); 
+             const MyMenu = (props) => (
+        		<Tabs>
+			<Tab  title="pageOne">
+					<Box name="widgetWrapperOne" 
+ width="100"> 
+<WidgetClassic data={statscovid}/>
+					</Box>
+			</Tab>
+			<Tab  title="pageTwo">
+					<Box name="widgetWrapperOne" 
+ width="100"> 
+<WidgetClassic data={statlicenciement}/>
+					</Box>
+			</Tab>
+		</Tabs>
 
-);
+        ); 
+            
+            const myTheme = {
+                global: { 
+                    colors: {black: '#000000',
+green: '#00FF00',
+white: '#FFFFFF',
+}}}
 
-const myTheme = {
-    global: {
-        colors: {
-            black: '#000000',
-            green: '#00FF00',
-            white: '#FFFFFF',
-        }
-    }
-}
-
-function App() {
-    return (
-        <Grommet plain>
-            <Box fill>
-
-                <AppBar>
-                    <Heading level='3' margin='none'>test</Heading>
-                </AppBar>
-            </Box>
-
-            <MyMenu></MyMenu>
-
-        </Grommet>
-    );
-}
-export default App;
+            function App() {
+                return(
+                    <Grommet plain>
+                        <Box fill>
+                            
+        <AppBar>
+            <Heading level='3' margin='none'>test</Heading>
+        </AppBar> 
+                        </Box>
+                        
+        <MyMenu></MyMenu> 
+        
+                    </Grommet>
+                );
+            }
+            export default App;
+        
--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
-import { CompositeGeneratorNode, processGeneratorNode} from 'langium';
+import { CompositeGeneratorNode, processGeneratorNode } from 'langium';
 import { extractDestinationAndName } from './cli-util';
 import path from 'path';
-import { App, Page, WidgetWrapper, Widget} from '../language-server/generated/ast';
+import { App, Page, WidgetWrapper, Widget } from '../language-server/generated/ast';
 import { StringBuilder } from '../utils/StringBuilder';
 
 export function generateJavaScript(app: App, filePath: string, destination: string | undefined): string {
@@ -25,13 +25,12 @@ export function generateJavaScript(app: App, filePath: string, destination: stri
 }
 
 class GrommetAppGenerator {
-
     /**
      * Entry point
      * @param app 
      * @returns code generated (app.js)
      */
-    compile(app: App): string{
+    compile(app: App): string {
         return `
             //groomet app generated : ${app.name}
             ${this.dependencies()}
@@ -58,7 +57,7 @@ class GrommetAppGenerator {
      * @returns dependencies as a string 
      * //todo le faire dynamiquement
      */
-    dependencies(): string{
+    dependencies(): string {
         return `import { Grommet, Box, Heading, Tabs, Tab, Image, Text } from 'grommet'; \n
                 import { statscovid, statlicenciement } from './data/data' \n`;
     }
@@ -69,35 +68,35 @@ class GrommetAppGenerator {
 
     /////////////////////////////// Generate Menu
 
-    defineTheme(app: App): string{
+    defineTheme(app: App): string {
         let sb: StringBuilder = new StringBuilder();
         sb.write(`
             const ${app.theme.name} = {
                 global: { 
                     colors: {`);
-        
-        `${app.theme.colors.forEach((c) => { 
+
+        `${app.theme.colors.forEach((c) => {
             sb.write(c.name);
             sb.write(": '");
             sb.write(c.code);
             sb.writeln("',");
         })}`;
-    
+
         sb.writeln("}}}");
         return sb.toString();
     }
 
     /////////////////////////////// Generate Header
 
-    generateHeader(app: App): string{
-        return `${app.header} `?`
+    generateHeader(app: App): string {
+        return `${app.header} ` ? `
         <${this.capitalizeFirstLetter(app.header.name)}>
             <Heading level='${app.header.level}' margin='none'>${app.header.title}</Heading>
-        </${this.capitalizeFirstLetter(app.header.name)}> `:` "s" `;
+        </${this.capitalizeFirstLetter(app.header.name)}> ` : ` "s" `;
     }
 
-    headerDeclaration(app: App): string{
-        return `${app.header}` ?` const ${this.capitalizeFirstLetter(app.header.name)} = (props) => (
+    headerDeclaration(app: App): string {
+        return `${app.header}` ? ` const ${this.capitalizeFirstLetter(app.header.name)} = (props) => (
             <Box
                 tag='header'
                 direction='row'
@@ -109,63 +108,63 @@ class GrommetAppGenerator {
                 style={{ zIndex: '1' }}
                 {...props}
             />
-        ); `:` "x"`;
+        ); `: ` "x"`;
     }
 
     ///////////////////////////////////MENU && WIDGET
-    generateMenu(app: App): string{
-        return `${app.menu} `?`
+    generateMenu(app: App): string {
+        return `${app.menu} ` ? `
         <${this.capitalizeFirstLetter(app.menu.name)}></${this.capitalizeFirstLetter(app.menu.name)}> 
         `:
-        ` "s" `;
+            ` "s" `;
     }
 
-    MenuDeclaration(app:App){
-        return `${app.menu}` ?` const ${app.menu.name} = (props) => (
+    MenuDeclaration(app: App) {
+        return `${app.menu}` ? ` const ${app.menu.name} = (props) => (
         ${this.pagesDeclaration(app.menu.pages)}
-        ); `:` "x"`;   
+        ); `: ` "x"`;
     }
 
-    pagesDeclaration(pages:Page[]){
+    pagesDeclaration(pages: Page[]) {
         let sb: StringBuilder = new StringBuilder();
         sb.write("\t\t<Tabs>\n")
-        pages.forEach((page)=>{
-            sb.write(`\t\t\t<Tab  title="${page.title}">\n`+ this.PageDeclaration(page) + "\t\t\t</Tab>\n")
+        pages.forEach((page) => {
+            sb.write(`\t\t\t<Tab  title="${page.title}">\n` + this.PageDeclaration(page) + "\t\t\t</Tab>\n")
         })
         sb.write("\t\t</Tabs>\n")
         return sb.toString()
     }
 
-    PageDeclaration(page: Page){
+    PageDeclaration(page: Page) {
         let pageWW = ""
-        page.widgetWrappers.forEach(widget=>{
+        page.widgetWrappers.forEach(widget => {
             pageWW += this.WidgetWrapperDeclaration(widget)
         })
         return pageWW;
     }
 
-    WidgetWrappersDeclaration(widgetWrappers: WidgetWrapper[]){
+    WidgetWrappersDeclaration(widgetWrappers: WidgetWrapper[]) {
         let widgetWrappersStr = "\t\t\t\t<Box> \n"
-        widgetWrappers.forEach(widget=>{
+        widgetWrappers.forEach(widget => {
             widgetWrappersStr += this.WidgetWrapperDeclaration(widget)
         })
         widgetWrappersStr += "\n\t\t\t\t</Box>"
         return widgetWrappersStr;
     }
-    
-    WidgetWrapperDeclaration(widgetWrapperObj: WidgetWrapper){
+
+    WidgetWrapperDeclaration(widgetWrapperObj: WidgetWrapper) {
         let widgets = `\t\t\t\t\t<Box name="${widgetWrapperObj.name}" \n width="${widgetWrapperObj.width}"> \n`
-        widgetWrapperObj.widgets.forEach(widgetObj=>{
+        widgetWrapperObj.widgets.forEach(widgetObj => {
             widgets += this.generateWidgets(widgetObj)
         })
         widgets += "\n\t\t\t\t\t</Box>\n"
         return widgets;
     }
 
-    WidgetDeclaration(widget?: Widget){
-        if(widget == undefined){
+    WidgetDeclaration(widget?: Widget) {
+        if (widget == undefined) {
             return ''
-        }else{
+        } else {
             return `
             \t\t\t\t\t\t<Box>
             \t\t\t\t\t\t\t title: ${widget.title}
@@ -175,16 +174,16 @@ class GrommetAppGenerator {
         }
     }
 
-    generateWidgets(widget: Widget): string{
-        if(`${widget.$type}` === "WidgetClassique"){ // widget classic
+    generateWidgets(widget: Widget): string {
+        if (`${widget.$type}` === "WidgetClassique") { // widget classic
             return this.generateWidgetClassic(widget);
-        }else{ // others types widget 
+        } else { // others types widget 
             console.log(widget);
             return "other type";
         }
     }
 
-    generateWidgetClassic(widget: Widget): string{
+    generateWidgetClassic(widget: Widget): string {
         return "<WidgetClassic data={" + `${widget.name}` + "}/>"
     }
 
@@ -195,31 +194,31 @@ class GrommetAppGenerator {
     generateWidgetClassicComponent(): string {
         let sb: StringBuilder = new StringBuilder();
         sb.writeln(this.declareConst("WidgetClassic"));
-        sb.writeln("<Box round pad=\"medium\" direction=\"column\" background=\"#EEEEEE\"> "); 
+        sb.writeln("<Box round pad=\"medium\" direction=\"column\" background=\"#EEEEEE\"> ");
         sb.writeln("<Box height=\"xsmall\" width=\"xsmall\">");
-        sb.writeln("<Image fit=\"cover\" src={data.icon_url}/> </Box>"); 
-        sb.writeln("<Heading alignSelf=\"center\" level=\"2\" margin=\"none\" size=\"small\"> {data.title} </Heading>"); 
-        sb.writeln("<Text alignSelf=\"center\" size=\"90px\" weight=\"bold\"> {data.value} </Text></Box>");
+        sb.writeln("<Image fit=\"cover\" src={data.icon_url}/> \n </Box>");
+        sb.writeln("<Heading alignSelf=\"center\" level=\"2\" margin=\"none\" size=\"small\"> {data.title} </Heading>");
+        sb.writeln("<Text alignSelf=\"center\" size=\"90px\" weight=\"bold\"> {data.value} </Text> \n </Box>");
         sb.write(");");
         return sb.toString();
     }
 
-    declarationComponents(app: App): string{
+    declarationComponents(app: App): string {
+        let widgets: Widget[] = app.menu.pages.map(p =>
+            p.widgetWrappers.flatMap(ww => ww.widgets)
+        ).flat();
+
         let isAlreadyVisited = true;
         let sb: StringBuilder = new StringBuilder();
-        `${app.menu.pages.forEach((page) => {
-            page.widgetWrappers.forEach((widgetWrapper) => {
-                widgetWrapper.widgets.forEach((widget) => {
-                    switch (widget.$type){
-                        case 'WidgetClassique':
-                            isAlreadyVisited ? sb.write(this.generateWidgetClassicComponent()) : "";
-                            isAlreadyVisited = false;
-                            break;
-                        case 'WidgetHisto':
-                            break;
-                    }
-                })
-            })
+        `${widgets.forEach(widget => {
+            switch (widget.$type) {
+                case 'WidgetClassique':
+                    isAlreadyVisited ? sb.write(this.generateWidgetClassicComponent()) : "";
+                    isAlreadyVisited = false;
+                    break;
+                case 'WidgetHisto':
+                    break;
+            }
         })}`;
         return sb.toString();
     }
