bada024a97ebd073f74047ffe3a59a0bdd060b01
fertala <54958141+fertala@users.noreply.github.com>
2022-03-01 14:18:03 +0100

    Update build.sh

--- scenarios/build.sh
+++ scenarios/build.sh
@@ -7,20 +7,18 @@ fi
 IFS='.' read -r -a titre <<< $1
 echo $titre
 
-cd ..
+ cd ..
 
 echo "Le scénario est "$titre
 echo "create app "$titre
 npx create-react-app $titre
-rm -rf yarn.lock
-rm -rf node_modules
 
 echo "copie des fichiers de configuration"
 cp -rf "example\responsive-app\package.json"  "$titre\package.json"
 cp -rf "example\responsive-app\src\data"  "$titre\src\data"
 echo "copie des fichiers génerer "
 
-cp -rf "scenarios\generated\test.js" "$titre\src\app.js"
+cp -rf 'scenarios\generated\'$titre".js" "$titre\src\app.js"
 cp -rf "scenarios\generated\components" "$titre\src\components"
 
 echo "installing the packages"
